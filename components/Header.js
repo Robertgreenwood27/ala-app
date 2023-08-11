544ce53a13d2f5da18711d3bf8975c330c8d6f02
Robert Greenwood <RobertGreenwood27@gmail.com>
2023-08-11 12:08:35 -0600

    first commit

--- components/Header.js
+++ components/Header.js
@@ -1,36 +1,38 @@
-// app/components/Header.js
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image'; // Import the Image component
 
 const Header = () => {
   return (
-    <header className="bg-white py-4 shadow">
+    <header className="bg-black py-1 shadow">
       <div className="container mx-auto flex items-center justify-between">
+        {/* Logo */}
         <Link href="/" legacyBehavior>
-          <a className="text-xl font-semibold text-gray-800">
-            Your Financial Cleanup
+          <a className="text-xl font-semibold text-white">
+            {/* You can adjust the width and height of the logo here */}
+            <Image src="/logo.png" alt="Logo" width={300} height={300} />
           </a>
         </Link>
         <nav>
           <ul className="flex space-x-6">
             <li>
               <Link href="/about" legacyBehavior>
-                <a className="text-gray-600 hover:text-gray-800">About</a>
+                <a className="text-gray-300 hover:text-white text-base md:text-xl">About</a>
               </Link>
             </li>
             <li>
               <Link href="/services" legacyBehavior>
-                <a className="text-gray-600 hover:text-gray-800">Services</a>
+                <a className="text-gray-300 hover:text-white text-base md:text-xl">Services</a>
               </Link>
             </li>
             <li>
               <Link href="/portfolio" legacyBehavior>
-                <a className="text-gray-600 hover:text-gray-800">Portfolio</a>
+                <a className="text-gray-300 hover:text-white text-base md:text-xl">Portfolio</a>
               </Link>
             </li>
             <li>
               <Link href="/contact" legacyBehavior>
-                <a className="text-gray-600 hover:text-gray-800">Contact</a>
+                <a className="text-gray-300 hover:text-white text-base md:text-xl">Contact</a>
               </Link>
             </li>
           </ul>
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,33 @@
 import React from 'react';
+import Image from 'next/image'; // Import the Image component
 
 const Home = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen p-8">
-  <h1 className="text-4xl font-semibold mb-4">
-    Trusted Accounting Professional Bringing Order to Financial Chaos
-  </h1>
-  <p className="text-lg text-center mb-8">
-    Specializing in cleanup services that provide clear financial benefits to clients.
-  </p>
-  {/* Image */}
-  <a
-    href="/about"
-    className="text-blue-500 hover:text-blue-600 text-lg font-semibold transition duration-300"
-  >
-    Learn More &rarr;
-  </a>
-</div>
-
+      {/* Profile Picture */}
+      <div className="mb-4">
+        <Image src="/business.png" alt="Accountant" width={200} height={200} />
+      </div>
+      <h1 className="text-4xl font-semibold mb-4">
+        Trusted Accounting Professional Bringing Order to Financial Chaos
+      </h1>
+      <p className="text-lg text-center mb-8">
+        Welcome to "Your Financial Cleanup," your trusted partner in achieving financial clarity and stability.
+        Our dedicated team of skilled professionals specializes in providing top-notch cleanup services that
+        bring clear and tangible financial benefits to our valued clients.
+      </p>
+      <p className="text-lg text-center mb-8">
+        With years of experience and a proven track record, we are committed to helping individuals and businesses
+        organize their financial records, streamline processes, and unlock new opportunities for growth.
+      </p>
+      {/* Call to Action */}
+      <a
+        href="/about"
+        className="text-blue-500 hover:text-blue-600 text-lg font-semibold transition duration-300"
+      >
+        Learn More &rarr;
+      </a>
+    </div>
   );
 };
 
